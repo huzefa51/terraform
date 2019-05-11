3a62c302426bee82477ef2a46fca7f6598575ef0
huzefa51 <huzefa.hamdard53@gmail.com>
2019-05-11 16:20:40 +0530

    Added Jenkins module

---
+++ main.tf
@@ -0,0 +1,73 @@
+# Create Jenkins Instance using Terraform
+# @author Huzefa Hamdard
+
+#Put terraform state file to S3
+terraform{
+    backend "s3"{
+    }
+}
+
+#Tell terraform that we will use AWS provider
+provider "aws"{
+    version = "~> 0.1"
+    region = "${var.region}"
+}
+
+provider "template"{
+    version = "~> 0.1"
+}
+
+#Set path to store terraform state file. 
+data "terraform_remote_state" "jenkins_state" {
+    backend = "s3"
+    config {
+        bucket = "${var.s3prefix}-terraform-states-${var.region}"
+        key = "${var.env_name}/jenkins.tfstate"
+        region = "${var.region}"
+    }
+}
+
+module "jenkins" {
+  source                      = "./modules/jenkins-master"
+
+  vpc_id                      = "${data.aws_vpc.default.id}"
+
+  name                        = "${var.name == "" ? "jenkins-master" : join("-", list(var.name, "jenkins-master"))}"
+  alb_prefix                  = "${var.name == "" ? "jenkins" : join("-", list(var.name, "jenkins"))}"
+  instance_type               = "${var.instance_type_master}"
+
+  ami_id                      = "${var.master_ami_id == "" ? data.aws_ami.jenkins.image_id : var.master_ami_id}"
+  user_data                   = ""
+  setup_data                  = "${data.template_file.setup_data_master.rendered}"
+
+  http_port                   = "${var.http_port}"
+  allowed_ssh_cidr_blocks     = ["0.0.0.0/0"]
+  allowed_inbound_cidr_blocks = ["0.0.0.0/0"]
+  ssh_key_name                = "${var.ssh_key_name}"
+  ssh_key_path                = "${var.ssh_key_path}"
+
+  # Config used by the Application Load Balancer
+  subnet_ids                  = "${data.aws_subnet_ids.default.ids}"
+  aws_ssl_certificate_arn     = "${var.aws_ssl_certificate_arn}"
+  dns_zone                    = "${var.dns_zone}"
+  app_dns_name                = "${var.app_dns_name}"
+}
+
+data "template_file" "setup_data_master" {
+  template = "${file("./modules/jenkins-master/setup.tpl")}"
+
+  vars = {
+    jnlp_port = "${var.jnlp_port}"
+    plugins = "${join(" ", var.plugins)}"
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = "${data.aws_vpc.default.id}"
+}
+
+
---
+++ modules/jenkins/main.tf
@@ -0,0 +1,42 @@
+resource "aws_instance" "jenkins" {
+  count                  = 1
+  ami                    = "${var.ami_id}"
+  instance_type          = "${var.instance_type}"
+  user_data              = "${var.user_data}"
+  key_name               = "${var.ssh_key_name}"
+  monitoring             = true
+  vpc_security_group_ids = ["${module.security_group_rules.jenkins_security_group_id}"]
+  tags                   = "${var.tag}"
+
+  provisioner "file" {
+    connection  = {
+      user = "ec2-user"
+      private_key = "${file(var.ssh_key_path)}"
+    }
+    content     = "${var.setup_data}"
+    destination = "/tmp/setup.sh"
+  }
+
+  provisioner "remote-exec" {
+    connection  = {
+      user = "ec2-user"
+      private_key = "${file(var.ssh_key_path)}"
+    }
+    inline = [
+      "chmod +x /tmp/setup.sh",
+      "sudo /tmp/setup.sh"
+    ]
+  }
+}
+
+module "security_group_rules" {
+  source = "../jenkins-security-group-rules"
+
+  name      = "${var.name}"
+  allowed_inbound_cidr_blocks = ["${var.allowed_inbound_cidr_blocks}"]
+  allowed_ssh_cidr_blocks = ["${var.allowed_ssh_cidr_blocks}"]
+
+  http_port = "${var.http_port}"
+  https_port = "${var.https_port}"
+  jnlp_port = "${var.jnlp_port}"
+}
